a841ecbafdb43fefea60af0ee08f3286d0b0d7dd
EndHere <39721036+EndHere@users.noreply.github.com>
2019-03-29 20:12:58 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  url: 'https://endhere.github.io/Maxine-Writes/,
+  title: 'Maxine Writes',
+  subtitle: '我想說的事',
+  copyright: 'CC-BY-SA-4.0',
+  disqusShortname: '',
+  postsPerPage: 10,
+  googleAnalyticsId: 'UA-99586914-3',
+  menu: [
+    {
+      label: 'Posts',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Maxine Maz',
+    photo: '/logo.jpg',
+    bio:
+      '喬編分身。我喜歡冷門的想法，所以多數人不理解或沒共鳴。這裡只是我隨筆整理思緒的地方。',
+    contacts: {
+      email: 'ovomis2u@gmail.com',
+      telegram: '#',
+      twitter: '@maxine_maz',
+      github: 'End Here',
+      rss: 'https://endhere.github.io/Maxine-Writes/rss.xml',
+      vkontakte: '#'
+    }
+  }
+};
